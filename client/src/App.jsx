13aed4850c7a62cbaad802085cbc0622e6806548
Monty Singer <Montster@Whisper.local>
2026-04-26 12:32:44 -0400

    Move toast notifications to bottom-right so they don't block Publish button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import EditorPanel from './components/editor/EditorPanel';
 export default function App() {
   return (
     <div className="flex h-screen overflow-hidden">
-      <Toaster position="top-right" toastOptions={{
+      <Toaster position="bottom-right" toastOptions={{
         style: { background: '#1C1F2B', color: '#E2E8F0', border: '1px solid #2A2D3A', fontSize: '13px' },
       }} />
       <main className="flex-1 overflow-auto">
